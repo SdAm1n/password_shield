1b06c9e44ccd0632a1a416b3c6db6a31f31f59a7
Sadikul Amin Sadman <anarchst49@gmail.com>
2023-03-27 02:03:45 +0600

    added some functions

--- main.cpp
+++ main.cpp
@@ -5,17 +5,53 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 using namespace std;
+
+// menu function shows menu for password manager
 
 void menu() {
     cout << "1. Generate Password" << endl;
     cout << "2. Store Password" << endl;
     cout << "3. Find Password"<<endl;
+    cout << "4. View All Passwords" << endl;
     cout << "q. Quit"<<endl;
 }
 
-// pseudo random password generator
+// store_password function stores password in password.txt file
+
+void store_password(string &default_password) {
+    ofstream pass_file("password.txt",ios::app);
+
+    if(!pass_file){
+        cerr << "Could not open the password file" << endl;
+        exit(EXIT_FAILURE);
+    }
+    string username_email, website, password;
+    cout << "Enter Username/Email, Website";
+    
+    if(!default_password.empty()) {
+        password = default_password;
+    }
+    else {
+        cout << ", Password: ";
+        cin>> password;
+    }
+    cout << endl;
+
+   
+    cin>> username_email >> website;
+
+    pass_file << username_email << " " << website << " "<< password << endl;
+    pass_file.close();
+
+    cout << "Password is stored" << endl;
+
+}
+
+// generate_password function is a pseudo random password generator 
+// which generates a random password 
 
 void generate_password() {
 
@@ -30,20 +66,42 @@ void generate_password() {
     int password_length;
     cin >> password_length;
     cout << "\nGenerating Password.......\n" <<endl;
+
+    string new_pass;
     while(password_length--){
-        cout << random_str[rand()%random_str_len];
+        // cout << random_str[rand()%random_str_len];
+        new_pass+=random_str[rand()%random_str_len];
     }
-    cout << endl <<endl;
-}
+    cout << new_pass << endl <<endl;
 
-void store_password() {
-
+    cout << "Do you want to save the password (Y or N): ";
+    char choice;
+    cin>>choice;
+    if(choice == 'Y'){
+        store_password(new_pass);
+    }
 }
 
 void find_password() {
 
 }
 
+void view_all_password() {
+    ifstream pass_file("password.txt", ios::in);
+
+    if(!pass_file){
+        cerr<< "Could not open the password file" <<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    string username_email, website, password;
+    while(pass_file >> username_email >> website >> password){
+        cout << username_email<< " " << website << " " << password << endl; 
+    }
+
+    pass_file.close();
+
+}
 
 int main(){
     char option;
@@ -55,10 +113,14 @@ int main(){
             generate_password();
         }
         else if (option == '2') {
-            store_password();
+            string default_password = "";
+            store_password(default_password);
         }
         else if(option == '3'){
             find_password();
+        }
+        else if(option == '4'){
+            view_all_password();
         }
         else if(option != 'q'){
             cout << "Wrong Option\nTry Entering 1, 2 or q :)\n" << endl;
